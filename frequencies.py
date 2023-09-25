856037dc3c59b727b2efecb31e3181d36fbaafa3
Johnathan Dawber <johnathan.dawber@kcl.ac.uk>
2023-09-25 03:55:48 -0700

    second

--- frequencies.py
+++ frequencies.py
@@ -4,9 +4,11 @@
 def frequencies(items):
     frequencies = {}
     for item in items:
+        item = str(item)
         if item in frequencies:
             frequencies[item]+=1
         else:
             frequencies[item]=1
     # Your code goes here
     return frequencies
+print(frequencies([100, 'Hello', '100', '100', 100]))
